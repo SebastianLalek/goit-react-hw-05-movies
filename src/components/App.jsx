633ce7f46e8ad8067d8db5cb76f66fae17f7f629
Sebastian Lalek <chemiczq@gmail.com>
2023-11-23 11:11:27 +0100

    add API request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
+import axios from 'axios';
+import { apiKey, apiUrl } from './consts/theMoviesDB';
+
+function Movies() {
+  const url = apiUrl;
+
+  const params = {
+    api_key: apiKey,
+    query: 'panda',
+  };
+  const films = async () => {
+    const response = await axios
+      .get(url, { params })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+    console.log(response);
+  };
+
+  console.log(films());
+}
+
+Movies();
+
 export const App = () => {
   return (
     <div
@@ -7,7 +30,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
---
+++ src/components/consts/theMoviesDB.js
@@ -0,0 +1,2 @@
+export const apiKey = process.env.REACT_APP_API_KEY;
+export const apiUrl = 'https://api.themoviedb.org/3/search/movie';
