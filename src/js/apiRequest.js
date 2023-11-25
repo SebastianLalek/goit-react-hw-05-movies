2d4893360c0f0b7aa6632eccb107e4772cf54fe8
Sebastian Lalek <chemiczq@gmail.com>
2023-11-25 10:57:24 +0100

    add apiRequest

---
+++ src/js/apiRequest.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { apiKey, apiUrl } from 'components/consts/theMoviesDB';
+
+export const apiRequest = async (endpoint, additionalParams) => {
+  const url = `${apiUrl}${endpoint}`;
+
+  const params = {
+    api_key: apiKey,
+    ...additionalParams,
+  };
+  const result = await axios
+    .get(url, { params })
+    .then(result => result.data.results)
+    .catch(err => console.log(err));
+
+  //   console.log(result);
+  return result;
+};
+
+// console.log(apiRequest('search/movie', { query: 'panda' }));
