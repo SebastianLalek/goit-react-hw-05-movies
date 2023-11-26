c74f609a785cb1ee3e9239fc3bd921e05ae57f76
Sebastian Lalek <123250026+SebastianLalek@users.noreply.github.com>
2023-11-26 15:10:15 +0100

    add searching movies by title

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,3 +1,41 @@
+import { apiRequest } from "js/apiRequest";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <h2>Movies</h2>;
+  const [films, setFilms] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    async function fetchFilms() {
+      if( searchQuery === '') {
+        return
+      }
+      const data = await apiRequest('search/movie', {query: searchQuery})
+      setFilms([...data])
+    }
+
+    fetchFilms()
+  } ,[searchQuery])
+
+function submitQuery(e) {
+  e.preventDefault();
+    const input = e.target.query.value;
+
+  setSearchQuery(input)
+}
+
+
+  return (
+  <>
+  <form onSubmit={submitQuery}>
+    <input name="query" placeholder="Search movies" />
+    <button>Search</button>
+  </form>
+
+  <ul>
+      {films.map(film => (
+        <li key={film.id}>{film.original_title}</li>
+      ))}
+    </ul>
+  </>);
 }
