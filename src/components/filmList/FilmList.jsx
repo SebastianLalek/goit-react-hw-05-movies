bc4684919cfd00e6e65288d289969421b62d0a29
Sebastian Lalek <chemiczq@gmail.com>
2023-11-27 20:49:38 +0100

    update Link props

--- src/components/filmList/FilmList.jsx
+++ src/components/filmList/FilmList.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
-import css from "./FilmList.module.css"
+import css from './FilmList.module.css';
 
-export default function FilmList({list}) {
-    return (
-        <ul className={css.list}>
-            {list.map(element => 
-                <li className={css.listItem} key={element.id}>
-                    <Link className={css.listLink} to={element.id}>{element.original_title}</Link>
-                </li>
-            )}
-        </ul>
-    )
+export default function FilmList({ list }) {
+  return (
+    <ul className={css.list}>
+      {list.map(element => (
+        <li className={css.listItem} key={element.id}>
+          <Link className={css.listLink} to={`/movies/${element.id}`}>
+            {element.original_title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
