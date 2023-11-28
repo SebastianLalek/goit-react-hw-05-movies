95af032d7fa9e1cfa2c76f956044a1b1abbb858c
Sebastian Lalek <chemiczq@gmail.com>
2023-11-28 21:20:08 +0100

    add MovieDetails page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,19 @@ import Header from './header/Header';
 import Home from 'pages/home/Home';
 import Movies from 'pages/movies/Movies';
 import NotFound from './notFound/NotFound';
+import MovieDetails from 'pages/movieDetails/MovieDetails';
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Suspense fallback={<h3>Loading...</h3>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Suspense>
     </div>
   );
---
+++ src/pages/movieDetails/MovieDetails.jsx
@@ -0,0 +1,39 @@
+import { apiRequest } from 'js/apiRequest';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function MovieDetails() {
+  const [movieDetails, setMovieDetails] = useState({});
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function fetchFilms() {
+      const data = await apiRequest(`movie/${movieId}`);
+      console.log(data);
+      setMovieDetails(data);
+    }
+
+    fetchFilms();
+  }, []);
+
+  const { original_title, overview, vote_average, poster_path, genres } =
+    movieDetails;
+
+  return (
+    <div>
+      {/* <img
+        src={`https://image.tmdb.org/t/p/original${poster_path}`}
+      /> */}
+      <h2>{original_title}</h2>
+      <p>User Score: {vote_average}</p>
+      <h3>Overview</h3>
+      <p>{overview} </p>
+      <h3>Genres</h3>
+      <p>
+        {/* {genres.map(genre => (
+          <span>{genre.name} </span>
+        ))} */}
+      </p>
+    </div>
+  );
+}
