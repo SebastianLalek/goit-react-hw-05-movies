0e2680660c5a6f38fa8fad41e8dde87f6860dbc8
Sebastian Lalek <chemiczq@gmail.com>
2023-11-29 17:18:58 +0100

    add Cast styles

--- src/components/cast/Cast.jsx
+++ src/components/cast/Cast.jsx
@@ -2,26 +2,35 @@ import { apiRequest } from 'js/apiRequest';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import css from './Cast.module.css';
+
 export default function Cast() {
   const [movieCast, setMovieCast] = useState([]);
   const { movieId } = useParams();
 
   useEffect(() => {
-    async function fetchFilms() {
+    async function fetchCredits() {
       const data = await apiRequest(`movie/${movieId}/credits`);
       console.log(data);
       setMovieCast([...data.cast]);
     }
 
-    fetchFilms();
+    fetchCredits();
   }, [movieId]);
   return (
     <div>
       <h4>Cast</h4>
-      <ul>
+      <ul className={css.list}>
         {movieCast.map(cast => (
-          <li>
-            {cast.name} - {cast.character}
+          <li className={css.item} key={cast.cast_id}>
+            <img
+              className={css.image}
+              src={`https://image.tmdb.org/t/p/original${cast.profile_path}`}
+            />
+            <div>
+              <p className={css.actor}>{cast.name}</p>
+              <p className={css.character}>{cast.character}</p>
+            </div>
           </li>
         ))}
       </ul>
